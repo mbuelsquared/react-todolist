dc0f1faf933de7a3ba9e220f6d72fc2f1aecec7b
mbuelsquared <mehmet.buelbuel34@gmail.com>
2024-12-07 15:04:24 +0100

    Implemented basic todo list functionality.

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -8,7 +8,7 @@ export default function TodoCard(props) {
         <div className='actionsContainer'>
 
             <button onClick={()=> {
-                handleEditTodo(index)
+                handleEditTodo(index )
             }}>
             <i className="fa-solid fa-pen-to-square"></i>
             </button>
